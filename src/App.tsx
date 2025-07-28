2c03e7dd6032eaab78ef2214e8e27dd050447127
mailpeters <mailpeters@gmail.com>
2025-07-28 15:27:36 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-
+  const { user, signOut } = useAuthenticator();
+  
   const { signOut } = useAuthenticator();
   
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
@@ -30,6 +31,9 @@ function App() {
   
   return (
     <main>
+
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
 
